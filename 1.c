577e1f4f811511a9721a0978abd0c57f7155e739
Stephen <859005086@qq.com>
2016-10-02 09:32:23 +0800

    testc

---
+++ 1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	int a = 0;
+	int b[10];
+	for(int i = 0;i < 10;i++){
+		b[i] = i+1;
+	}
+	for(int i = 0;i < 10;i++){
+		a += b[i];
+	}
+	printf("%d\n", a);
+	return 0;
+}
